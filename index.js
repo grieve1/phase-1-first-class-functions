4d21260a020da3e3bd221426b52b59b4ca3703ce
drake bishop <drakemx85@gmail.com>
2023-02-20 21:01:40 -0600

    learned first class functions

---
+++ index.js
@@ -0,0 +1,16 @@
+function receivesAFunction(callback) {
+    callback();
+  }
+
+function returnsANamedFunction() {
+    function namedFunction() {
+      console.log("Hello, world!");
+    }
+    return namedFunction;
+  }
+
+function returnsAnAnonymousFunction() {
+    return function() {
+      console.log("Hello, world!");
+    };
+  }
